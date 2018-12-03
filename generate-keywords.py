0b62768117b2a20cd1772909e3aa46225f47fa46
Daniel Simandl <dsimandl419@gmail.com>
2018-12-02 23:00:25 -0600

    script writes keywords to db

--- generate-keywords.py
+++ generate-keywords.py
@@ -1,56 +1,72 @@
+#! /usr/bin/env python3
+from urllib.request import urlopen, Request
+from pymongo import MongoClient
 import textract
-import sys
 import os
 import requests
 import pandas as pd
 from pprint import pprint
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer, TfidfTransformer
 from nltk.corpus import stopwords
-sys.path.append("./utils")
 from helper_functions import *
-from pymongo import MongoClient
-from urllib.request import urlopen, Request
+from bson.objectid import ObjectId
 
 # mongoDB = os.environ['MONGO_DB']
 mongoDB = 'mongodb://192.168.99.100/sedaily'
 client = MongoClient(mongoDB)
 db = client.get_database()
 # posts = db.posts.find()
-posts = requests.get('http://192.168.99.100:4040/api/posts?transcripts=true&limit=3').json()
+
+posts = requests.get(
+    'http://192.168.99.100:4040/api/posts?transcripts=true&limit=1').json()
 
 print(len(posts))
+
+custom_stopwords = get_stopwords()
+
+articles = pd.read_pickle('./pickle-files/preprocessed_docs.pkl')
+
+print('length of articles', len(articles))
+
+df_articles = pd.DataFrame(articles)
+
+corpus = df_articles['full_text'].tolist()
+
+vectorizer = TfidfVectorizer(
+    max_df=0.85, smooth_idf=True, use_idf=True, stop_words=custom_stopwords)
+X = vectorizer.fit_transform(corpus)
+
+# get feature to index mapping
+feature_names = vectorizer.get_feature_names()
+
+# generate tf-idf for the given document
 for post in posts:
-  transcriptUrl = post['transcriptUrl']
-  _id = post['_id']
-  print(transcriptUrl, _id)
-  print(pre_process_transcript(transcriptUrl))
+    keywords = {}
+    transcriptUrl = post['transcriptUrl']
+    postId = post['_id']
+    tags = post['filterTags']
+    print('mongoDb _id', postId, '\n', 'tags: ', tags, '\n')
+    (filename, text) = pre_process_transcript(transcriptUrl)
+    # os.remove(filename)
 
-# custom_stopwords = get_stopwords()
+    tf_idf_vector = vectorizer.transform([text])
 
-# articles = pd.read_pickle('./pickle-files/preprocessed_docs.pkl')
-
-# print('length of articles', len(articles))
-
-# df_articles = pd.DataFrame(articles)
-
-# corpus = df_articles['full_text'].tolist()
-
-# vectorizer = TfidfVectorizer(max_df=0.85, smooth_idf=True, use_idf=True,stop_words=custom_stopwords)
-# X = vectorizer.fit_transform(corpus)
-
-# # get feature to index mapping
-# feature_names = vectorizer.get_feature_names()
-
-# #generate tf-idf for the given document
-# tf_idf_vector = vectorizer.transform([test])
-
-# #sort the tf-idf vectors by descending order of scores
-# sorted_items = sort_coo(tf_idf_vector.tocoo())
-# print('sorted',sorted_items[:30])
-# #extract only the top n; n here is 10
-# keywords = extract_topn_from_vector(feature_names,sorted_items,20)
-# print('raw keywords', keywords)
-# # now print the results
-# print("\n===Keywords===")
-# for k in keywords:
-#     print(k,keywords[k])
+    # sort the tf-idf vectors by descending order of scores
+    sorted_items = sort_coo(tf_idf_vector.tocoo())
+    print('sorted', sorted_items[:30])
+    # extract only the top n; n here is 10
+    keywords = extract_topn_from_vector(feature_names, sorted_items, 30)
+    for tag in tags:
+        keywords[tag['slug']] = 1.0
+    print('raw keywords', keywords)
+    print("\n===Text===")
+    print(filename, text)
+    # now print the results
+    print("\n===Keywords===")
+    for k in keywords:
+        print(k, keywords[k])
+    try:
+        db.posts.update({'_id': ObjectId(post['_id'])}, {'$set': { 'keywords': keywords }}, upsert=False, multi=False)
+    except:
+        print('failed to update')
+    db.close()
--- helper_functions.py
+++ helper_functions.py
@@ -1,4 +1,4 @@
-#! /bin/usr
+#! /usr/bin/env python3
 import re
 from nltk import pos_tag
 from nltk.tokenize import word_tokenize
@@ -13,24 +13,28 @@ from urllib.request import urlopen, Request, urlretrieve
 
 lemma = WordNetLemmatizer()
 
+
 def get_stopwords():
-  with open('./utils/stopwords.txt') as f:
-    additional_stopwords = f.read().splitlines()
+    with open('./utils/stopwords.txt') as f:
+        additional_stopwords = f.read().splitlines()
 
-  return set(stopwords.words('english') + list(additional_stopwords))
+    return set(stopwords.words('english') + list(additional_stopwords))
 
-def save_obj(obj, name ):
+
+def save_obj(obj, name):
     with open('./pickle-files/' + name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
 
 def sort_coo(coo_matrix):
     tuples = zip(coo_matrix.col, coo_matrix.data)
     return sorted(tuples, key=lambda x: (x[1], x[0]), reverse=True)
 
+
 def extract_topn_from_vector(feature_names, sorted_items, topn=20):
     """get the feature names and tf-idf score of top n items"""
 
-    #use only topn items from vector
+    # use only topn items from vector
     sorted_items = sorted_items[:topn]
 
     score_vals = []
@@ -39,17 +43,18 @@ def extract_topn_from_vector(feature_names, sorted_items, topn=20):
     # word index and corresponding tf-idf score
     for idx, score in sorted_items:
 
-        #keep track of feature name and its corresponding score
+        # keep track of feature name and its corresponding score
         score_vals.append(round(score, 3))
         feature_vals.append(feature_names[idx])
 
-    #create a tuples of feature,score
+    # create a tuples of feature,score
     #results = zip(feature_vals,score_vals)
-    results= {}
+    results = {}
     for idx in range(len(feature_vals)):
-        results[feature_vals[idx]]=score_vals[idx]
+        results[feature_vals[idx]] = score_vals[idx]
 
     return results
+
 
 def get_wordnet_pos(tag):
     if tag.startswith('J'):
@@ -62,11 +67,14 @@ def get_wordnet_pos(tag):
         return wn.ADV
     else:
         return wn.VERB
+
+
 def download_pdf(url):
     formattedUrl = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
     response = urlopen(formattedUrl)
     filename = './tmp/' + url.split('/')[7]
     return save_pdf(response, filename)
+
 
 def save_pdf(pdf, filename):
     file = open(filename, 'wb')
@@ -74,18 +82,21 @@ def save_pdf(pdf, filename):
     file.close()
     return filename
 
-def pre_process_transcript (url):
+def pre_process_transcript(url):
     filename = download_pdf(url)
     text = textract.process(filename, encoding='utf_8').decode('utf-8')
     text = text.replace('\n', ' ')
     text = text.replace('\n', ' ')
     text = text.lower()
-    text = re.sub("(\\d|\\W|\\_)+"," ",text)
-    text = re.sub("software engineering daily|transcript|introduction"," ",text)
-    text = re.sub('(sponsor message).*?(interview)'," ",text)
-    text = re.sub('(end of interview).+'," ",text)
+    text = re.sub("(\\d|\\W|\\_)+", " ", text)
+    text = re.sub(
+        "software engineering daily|transcript|introduction", " ", text)
+    text = re.sub('(sponsor message).*?(interview)', " ", text)
+    text = re.sub('(end of interview).+', " ", text)
     tokens = word_tokenize(text)
     pos_tokens = pos_tag(tokens)
-    lemmatized_tokens = [lemma.lemmatize(token[0], pos=get_wordnet_pos(token[1])) for token in pos_tokens]
-    text = " ".join(word for word in lemmatized_tokens if word not in get_stopwords() and len(word) > 3)
-    return text
+    lemmatized_tokens = [lemma.lemmatize(
+        token[0], pos=get_wordnet_pos(token[1])) for token in pos_tokens]
+    text = " ".join(
+        word for word in lemmatized_tokens if word not in get_stopwords() and len(word) > 3)
+    return filename, text
